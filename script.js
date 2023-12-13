5ddb40460cb3098a0a59d8777e90f134fe533862
DariaTkachynska <128737731+DariaTkachynska@users.noreply.github.com>
2023-12-13 02:35:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenuButton = document.getElementById('mobile-menu');
+    const menu = document.querySelector('.menu');
+
+    mobileMenuButton.addEventListener('click', function () {
+        menu.classList.toggle('active');
+    });
+});
